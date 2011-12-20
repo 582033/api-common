4d295a0f1303feb4cd589d40e8262be06b43e967
Ping Yin <ping@eee168.com>
2011-12-20 18:51:22 +0800

    Add retrieve_remote_file_size

--- helpers/MY_url_helper.php
+++ helpers/MY_url_helper.php
@@ -84,3 +84,20 @@ function post_request($url, $data) { //{{{
 	$result = file_get_contents($url, false, $context);
 	return $result;
 } //}}}
+
+function retrieve_remote_file_size($url) { //{{{
+	$ch = curl_init($url);
+	curl_setopt($ch, CURLOPT_NOBODY, true);
+	curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+	curl_setopt($ch, CURLOPT_HEADER, true);
+	curl_setopt($ch, CURLOPT_FOLLOWLOCATION, true);
+	$data = curl_exec($ch);
+	curl_close($ch);
+	$contentLength = 0;
+	if ($data !== false) {
+		if (preg_match('/Content-Length: (\d+)/', $data, $matches)) {
+			$contentLength = (int) $matches[1];
+		}
+	}
+	return $contentLength;
+} //}}}
