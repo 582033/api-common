3c5e769ba4f13532665ab9a3d0db1c3ae1c6cd38
Ping Yin <ping@eee168.com>
2012-02-27 17:38:28 +0800

    ltitem helper: add get_app_dir_name

--- helpers/ltitem_helper.php
+++ helpers/ltitem_helper.php
@@ -14,3 +14,16 @@ function get_app_icon_path($package, $label) {
 	$icon_path = "$prefix/$lpackage-$label_md5.png";
 	return $icon_path;
 }
+
+function get_app_dir_name($package, $label, $version_code) {
+	/**
+	  return - package-label_md5[0:4]-vcode
+	 */
+	$lpackage = trim(strtolower($package));
+	$parts = explode('.', $lpackage);
+	$prefix = substr($parts[1], 0, 2);
+	$llabel = trim(strtolower($label));
+	$label_md5 = substr(md5($llabel), 0, 4);
+
+	return "$lpackage-$label_md5-$version_code";
+}
