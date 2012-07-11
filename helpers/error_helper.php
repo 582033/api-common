9b340e4814cbe29cea68b81ee062e05c2e0820ab
Yan Jiang <yjiang@eee168.com>
2012-07-11 11:36:19 +0800

    error_helper: Add show_error_text

--- helpers/error_helper.php
+++ helpers/error_helper.php
@@ -1,10 +1,20 @@
 <?php
-function show_error_json($errcode, $errmsg) {
-	$data = array(
-		'errcode' => $errcode, 
-		'errmsg' => $errmsg,
-	);
-	header("Content-type: application/json");
-	echo json_encode($data);
-	exit;
+if ( ! function_exists('show_error_json')) {
+	function show_error_json($errcode, $errmsg) { // {{{
+		$data = array(
+			'errcode' => $errcode, 
+			'errmsg' => $errmsg,
+		);
+		header("Content-type: application/json");
+		echo json_encode($data);
+		exit;
+	} //}}}
+}
+
+if ( ! function_exists('show_error_text')) {
+	function show_error_text($status_code, $message="") { //{{{
+		header("HTTP/1.1 $status_code");
+		if ($message) echo $message;
+		exit;
+	} //}}}
 }
